422cd426dc2b6bcacf5ae41f2ca4aa12425693dd
rafeehcp <rafeeh@blend-ed.com>
2024-09-21 07:05:27 +0000

    Refactor home page components and imports
    - Update font imports and remove unused font files
    - Move ProgressCard and Updates components to separate files
    - Update imports in AllRoutes.jsx to reflect new file locations
    - Remove unused home component file
    - Update imports in index.scss to include new page and component files
    - Update imports in UpcomingEvents and RecentActivity components to reflect new component file locations
    - Update imports in EventCard and RecentActivityCard components to reflect new component file locations
    - Update imports in DashboardIndex.jsx to reflect new component file locations

--- src/pages/calendar/EventDrawer.jsx
+++ src/pages/calendar/EventDrawer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Drawer, Icon } from '@blend-ed/blendx-ui';
 import { Link } from 'react-router-dom';
+import EventCard from '../../components/EventCard';
 
 const EventDrawer = ({ showDrawer, handleCloseDrawer, todaysEvents, upcomingEvents }) => {
     return (
@@ -11,25 +12,7 @@ const EventDrawer = ({ showDrawer, handleCloseDrawer, todaysEvents, upcomingEven
                     <div className="events-list-title">Today</div>
                     <div className="events-list-content">
                         {todaysEvents.length > 0 ? todaysEvents.map((event, index) => (
-                            <div key={index} className={`event-item event-${event.variant}`}>
-                                <div className="event-item-header">
-                                    <div className="event-item-date">
-                                        <Icon icon='calendar' type='line' className="event-item-icon" size='md' />
-                                        {new Date(event.date).toLocaleDateString()}
-                                    </div>
-                                    <div className="event-item-time">
-                                        <Icon icon='time' type='line' className="event-item-icon" size='md' />
-                                        {new Date(event.date).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' })}
-                                    </div>
-                                </div>
-                                <div className="event-item-body">
-                                    <div className="event-item-title">{event.title}</div>
-                                    {event.link && <Link className="event-item-link" to={event.link}>
-                                        <Icon icon='link' className="event-item-icon" size='md' />
-                                        join meeting
-                                    </Link>}
-                                </div>
-                            </div>
+                            <EventCard key={index} title={event.title} date={event.date} variant={event.variant} link={event.link} />
                         ))
                             : <div className="no-events">No events today</div>}
                     </div>
